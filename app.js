3a88ad318b9a504ab0f83bcc814e2b0a667d93dd
Livia Rosario <152235212+livsXC@users.noreply.github.com>
2024-11-07 13:35:30 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+alert('Boas-Vidas ao Jogo do número secreto');
+let numeroSecreto = 5;
+console.log(numeroSecreto)
+let chute;
+let tentativas = 1;
+
+// enquanto chute não for igual ao n.s.
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 100');
+    // se chute for igual ao número secreto
+    if (chute == numeroSecreto) {
+        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
+        } else {
+            alert(`O número secreto é maior que ${chute}`);
+        }
+        // tentaivas = tentaivas +1;
+        tentativas++;
+    }
+}
